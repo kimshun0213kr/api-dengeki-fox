f365c729bf04c8a01eabeb0c5e394630928b2b06
kimshunKR <96727208+kimshun0213kr@users.noreply.github.com>
2024-01-23 23:58:30 +0900

    Update: リダイレクトを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("https://dengeki-fox.net/");
+    }, 1000);
+  });
   return (
     <>
       <Head>
@@ -14,7 +23,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
-        <h1>HOME</h1>
+        <h1>Login not allowed.</h1>
+        <h3>
+          If you are not redirected, click{" "}
+          <Link as="span" href="https://dengeki-fox.net/">
+            here
+          </Link>{" "}
+          to access the home page.
+        </h3>
       </body>
     </>
   );
